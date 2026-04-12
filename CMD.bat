197b900078a7bc357a8d2eeee2584ed919ad36c0
David2019 <wiuwwror@gmail.com>
2026-04-12 15:43:33 +0800

    make cmd batch dev launcher only

--- CMD.bat
+++ CMD.bat
@@ -2,15 +2,9 @@
 setlocal
 cd /d "%~dp0"
 
-set "APP_EXE=%~dp0dist\CloudHime.exe"
 set "APP_PY=%~dp0CloudHime.py"
 set "PYTHONW=C:\Users\USER\miniconda3\pythonw.exe"
 set "PYTHON=C:\Users\USER\miniconda3\python.exe"
-
-if exist "%APP_EXE%" (
-    start "" "%APP_EXE%"
-    exit /b 0
-)
 
 if exist "%PYTHONW%" (
     start "" "%PYTHONW%" "%APP_PY%"
@@ -22,9 +16,8 @@ if exist "%PYTHON%" (
     exit /b 0
 )
 
-echo No available launcher found.
-echo 1. dist\CloudHime.exe
-echo 2. C:\Users\USER\miniconda3\pythonw.exe
-echo 3. C:\Users\USER\miniconda3\python.exe
+echo No Python launcher found.
+echo 1. C:\Users\USER\miniconda3\pythonw.exe
+echo 2. C:\Users\USER\miniconda3\python.exe
 pause
 exit /b 1
